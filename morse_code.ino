1162fd4ca39a7b9a952dd2caeab3038992d2c653
cberry216 <christopherberry216@gmail.com>
2018-07-25 19:30:27 -0400

    Code to enter and print Morse code

--- morse_code.ino
+++ morse_code.ino
@@ -1,34 +1,40 @@
-int push_pin = 2;
-
 void setup() {
     Serial.begin(9600);
+    pinMode(3, OUTPUT);
+
 }
 
 void loop() {
-    Serial.print("H");
-    delay(500);
-    Serial.print("e");
-    delay(500);
-    Serial.print("l");
-    delay(500);
-    Serial.print("l");
-    delay(500);
-    Serial.print("o");
-    delay(500);
-    Serial.print(" ");
-    delay(500);
-    Serial.print("M");
-    delay(500);
-    Serial.print("i");
-    delay(500);
-    Serial.print("c");
-    delay(500);
-    Serial.print("h");
-    delay(500);
-    Serial.print("a");
-    delay(500);
-    Serial.print("e");
-    delay(500);
-    Serial.println("l");
-    delay(500);
+    int switch_state = digitalRead(5);
+    if(switch_state == HIGH) {
+        unsigned int start_time = millis();
+        //Serial.println(start_time);
+        while((switch_state = digitalRead(5)) == HIGH) {
+            tone(3, 700, 20);
+        }
+        unsigned int end_time = millis();
+        //Serial.println(end_time);
+        unsigned int total_time = end_time - start_time;
+
+        if(total_time > 0 && total_time <= 200) {
+            Serial.print('.');
+        }
+        if(total_time > 200 && total_time <= 450) {
+            Serial.print('-');
+        }
+    }
+    else {
+        unsigned int start_time = millis();
+        while((switch_state = digitalRead(5)) == LOW) {}
+        unsigned int end_time = millis();
+        unsigned int total_time = end_time - start_time;
+        if(total_time > 500) {
+            Serial.print(' ');
+        }
+    }
+    delay(1);
+}
+
+char convert_time_to_char(int time) {
+    
 }
