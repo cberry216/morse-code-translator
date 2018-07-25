46746794ec39a71a3cc1e22fbd6c5ac01e05e893
cberry216 <christopherberry216@gmail.com>
2018-07-25 13:45:20 -0400

    initial commit

---
+++ morse_code.ino
@@ -0,0 +1,34 @@
+int push_pin = 2;
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    Serial.print("H");
+    delay(500);
+    Serial.print("e");
+    delay(500);
+    Serial.print("l");
+    delay(500);
+    Serial.print("l");
+    delay(500);
+    Serial.print("o");
+    delay(500);
+    Serial.print(" ");
+    delay(500);
+    Serial.print("M");
+    delay(500);
+    Serial.print("i");
+    delay(500);
+    Serial.print("c");
+    delay(500);
+    Serial.print("h");
+    delay(500);
+    Serial.print("a");
+    delay(500);
+    Serial.print("e");
+    delay(500);
+    Serial.println("l");
+    delay(500);
+}
